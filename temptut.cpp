cde522ddbca05597edd81c2bfc7999170e77145e
Atik Mansuri <atikmansuri619@gmail.com>
2021-10-27 09:05:04 +0530

    "temp file edited"

--- temptut.cpp
+++ temptut.cpp
@@ -1,12 +1,21 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
+
 using namespace std;
 
 int main() {
 
-    ofstream out;
+    int i;
 
-    out.open("E:/Angela telegram/Hello aut5555.txt");
+    for(i=0;i<10;i++) {
+
+        int b = 2;
+
+        cout<<i<<" = "<<b<<endl;
+
+            b++;
+    }
 
 
-
+        return 0;
 }
